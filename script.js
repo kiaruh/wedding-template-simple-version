d905fa7b8504fd544513cc1e075393364f85beab
kiaruh <nikoz.li@gmail.com>
2023-08-27 15:32:35 -0300

    count down

--- script.js
+++ script.js
@@ -34,3 +34,35 @@ window.onload = function () {
 window.addEventListener("resize", function () {
   updateElementVisibility();
 });
+
+// 
+// Function to calculate and update the countdown
+function updateCountdown() {
+  // Set the target date (December 9, 2030)
+  const targetDate = new Date('2023-12-09T00:00:00');
+
+  // Get the current date and time
+  const currentDate = new Date();
+
+  // Calculate the time remaining in milliseconds
+  const timeRemaining = targetDate - currentDate;
+
+  // Calculate days, hours, minutes, and seconds
+  const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+  // Update the HTML elements with the calculated values
+  document.querySelector('._172').textContent = days;
+  document.querySelector('._12').textContent = hours;
+  document.querySelector('._9').textContent = minutes;
+  document.querySelector('._18').textContent = seconds;
+}
+
+// Call the updateCountdown function initially
+updateCountdown();
+
+// Update the countdown every second
+setInterval(updateCountdown, 1000);
+
